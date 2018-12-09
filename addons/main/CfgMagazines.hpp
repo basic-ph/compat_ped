1caa6326b4dff689d42232f7f920de3f8c76a896
basic-ph <weirdbasic@hotmail.com>
2018-12-09 19:19:45 +0100

    replaced macros with full vars in CfgWeapons

--- addons/main/CfgMagazines.hpp
+++ addons/main/CfgMagazines.hpp
@@ -1,21 +1,6 @@
 
 class CfgMagazines {
 
-    class RPG32_F;
-    class ASZ_PZF3_Mag: RPG32_F {
-        EGVAR(overpressure,priority) = 99;
-        EGVAR(overpressure,angle) = 0;
-        EGVAR(overpressure,range) = 0;
-        EGVAR(overpressure,damage) = 0;
-    };
-
-    class CA_LauncherMagazine;
-    class ASZ_Stinger_Mag: CA_LauncherMagazine {
-        EGVAR(overpressure,angle) = 45;
-        EGVAR(overpressure,range) = 15;
-        EGVAR(overpressure,damage) = 0.3;
-    };
-
     class 30Rnd_556x45_Stanag;
     class 200Rnd_M249mag: 30Rnd_556x45_Stanag {
         ace_isbelt = 1;
--- addons/main/CfgWeapons.hpp
+++ addons/main/CfgWeapons.hpp
@@ -1,30 +1,30 @@
 
-//EGVAR(hearing,protection) = 0.85; EGVAR(hearing,lowerVolume) = 0.6;
-//EGVAR(hearing,protection) = 0.75; EGVAR(hearing,lowerVolume) = 0;
+//ace_hearing_protection = 0.85; ace_hearing_lowerVolume = 0.6;
+//ace_hearing_protection = 0.75; ace_hearing_lowerVolume = 0;
 class CfgWeapons {
     class ItemCore;
     class ASZ_VTG_Stingray_ACH_Helmet_base_Vegecam: ItemCore {
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0;
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0;
     };
     class ASZ_VTG_Stingray_ACH_Helmet_Vegecam : ItemCore {
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0;
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0;
     };
     class ASZ_mich_2000_vegecam : ItemCore { //sons: desert, vegecam, wood
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0
     };
     class ASZ_mich_2000_multiland : ItemCore {
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0
     };
     class ASZ_mich_2002_green : ItemCore {
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0
     };
     class ASZ_mich_2002_desert : ItemCore {
-        EGVAR(hearing,protection) = 0.75;
-        EGVAR(hearing,lowerVolume) = 0
+        ace_hearing_protection = 0.75;
+        ace_hearing_lowerVolume = 0
     };
 };
